0f30d4c212830e3a423b48dde20bbeb6fcf4158a
leoMurphy <shadyslim713@gmail.com>
2023-12-12 15:55:21 +0630

    change warping place of react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hello World</h1>,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span>This is Login</span>
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={routes}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>This is Login</span>
+          </a>
+        </header>
+      </div>
+    </RouterProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,22 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const routes = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world</div>,
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <RouterProvider router={routes}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </RouterProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
