aa32872aa517c9b0ceefbc803ba50ff669271187
leoMurphy <shadyslim713@gmail.com>
2023-12-12 15:47:39 +0630

    change file name and re-add strict mode

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter , RouterProvider } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const routes = createBrowserRouter([
   {
-    path:'/',
-    element:<div>Hello world</div>
-  }
-]) 
+    path: "/",
+    element: <div>Hello world</div>,
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <RouterProvider router={routes}>
+  <RouterProvider router={routes}>
+    <React.StrictMode>
       <App />
-    </RouterProvider>
+    </React.StrictMode>
+  </RouterProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
