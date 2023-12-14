cc5956c562fc698bbdc90d8686d2bc0d74ad7106
leoMurphy <shadyslim713@gmail.com>
2023-12-14 15:06:55 +0630

    adding home page route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Sidebar from "./pages/sidebar/sidebar";
 import Login from "./pages/login/login";
+import HomePage from "./pages/home/home"
 
 function App() {
   return (
      <Routes>
       <Route path="/login" element={<Login/>}></Route>
       <Route path="/sidebar" element={<Sidebar/>}></Route>
+      <Route path="/home" element={<HomePage/>}></Route>
      </Routes>
      
   );
