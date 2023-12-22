deac856ef2e1869e10d78f6e05551725d045c75e
leoMurphy <shadyslim713@gmail.com>
2023-12-22 10:47:39 +0630

    adding basic interceptor

--- src/utils/axiosApi.js
+++ src/utils/axiosApi.js
@@ -4,4 +4,24 @@ const axiosApi = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
+axiosApi.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosApi.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+
+  (error) => {
+    return Promise.reject(error);
+  }
+)
+
 export default axiosApi;
