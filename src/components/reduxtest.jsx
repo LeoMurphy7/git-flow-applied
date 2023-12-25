b499ce1c73dcbd8e6ea95d09445b58ac7eafd0f1
leoMurphy <shadyslim713@gmail.com>
2023-12-25 20:44:32 +0630

    dispatch outside of the component

--- src/components/reduxtest.jsx
+++ src/components/reduxtest.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { increment , decrement} from "../redux/slices/counterSlice";
+import { IncrementCounterSlice , DecrementCounterSlice } from "../utils";
 
 const ReduxTest = () => {
   const count = useSelector((state)=> state.counter.count);
@@ -11,14 +12,14 @@ const ReduxTest = () => {
       <div>
         <button
           aria-label="Increment value"
-          onClick={()=> dispatch(increment())}
+          onClick={()=> IncrementCounterSlice()}
         >
           Increment
         </button>
         <span>{count}</span>
         <button
           aria-label="Decrement value"
-          onClick={()=> dispatch(decrement())}
+          onClick={()=> DecrementCounterSlice()}
         >
           Decrement
         </button>
