e6ee6785636f8ad73fafe174500f694daee878db
leoMurphy <shadyslim713@gmail.com>
2023-12-26 13:14:57 +0630

    code refactoring and change the flow of using formik

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,80 +1,73 @@
-import React, { useState } from "react";
+import React from "react";
+import { RootRoute } from "../../routes";
 import { useAuth } from "../../utils/auth";
 import { useLocation, useNavigate } from "react-router";
-import { useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 export default function Login() {
-  // const [user, setUser] = useState({});
   const auth = useAuth();
   const navigate = useNavigate();
   const location = useLocation();
-  const redirectPath = location.state?.path || "/";
-
-  // const handleChange = (event) => {
-  //   setUser((prev)=> ({...prev, ["email"]: event.target.value }));
-  //   console.log('checking form input..',user)
-  // };
+  const redirectPath = location.state?.path || RootRoute;
 
   const handleLogin = (value) => {
-    // auth.login(user);
     auth.login(value);
     navigate(redirectPath, { replace: true });
   };
 
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-    },
-    validationSchema: Yup.object({
-      email: Yup.string().email("Invalid Email Address").required("Required"),
-      password: Yup.string().required("Reqired"),
-    }),
-    onSubmit: (values) => {
-      handleLogin(values.email);
-    },
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object({
+    email: Yup.string().email("Invalid Email Address").required("Required"),
+    password: Yup.string().required("Required"),
   });
 
+  const onSubmit = (values) => {
+    handleLogin(values.email);
+  };
+
   return (
-    <div className="card w-25 mx-auto">
-      <div className="card-body">
+    <Formik
+      className="card w-25 mx-auto"
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <div className="card-body w-25 mx-auto">
         <h5 className="card-title">Login</h5>
-        <form onSubmit={formik.handleSubmit}>
+        <Form>
           <div className="mb-2 d-flex align-items-center">
             <label htmlFor="email" className="card-text">
               Username:
             </label>
-            <input
+            <Field
               type="text"
               id="email"
+              name="email"
               className="form-control"
-              onChange={formik.handleChange}
-              value={formik.values.email}
             />
-            {formik.errors.email ? (
-              <span className="text-warning">{formik.errors.email}</span>
-            ) : null}
+            <ErrorMessage name="email" />
           </div>
 
           <div className="mb-2 d-flex align-items-center">
             <label htmlFor="password">Password: </label>
-            <input
+            <Field
               className="form-control"
               type="password"
               id="password"
               name="password"
-              {...formik.getFieldProps("password")}
             />
-            {formik.touched.password && formik.errors.password ? (
-              <span className="text-warning">{formik.errors.password}</span>
-            ) : null}
+            <ErrorMessage name="password" />
           </div>
           <button className="btn btn-primary" type="submit">
             Login
           </button>
-        </form>
+        </Form>
       </div>
-    </div>
+    </Formik>
   );
 }
